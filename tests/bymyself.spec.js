44feaadb40b07d447a88e2cadf2c9f6f69f6a4a9
Dmitry Kukarin <dmitrykukarin@macbook-air-dmitry.home>
2024-04-25 01:39:46 +0200

    first-commit

---
+++ tests/bymyself.spec.js
@@ -0,0 +1,21 @@
+const { test, expect } = require('@playwright/test');
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://demo.playwright.dev/todomvc');
+  });
+
+  const TODO_ITEMS = [
+    'buy some cheese',
+    'feed the cat',
+    'book a doctors appointment'
+  ];
+
+  test.describe('New Todo', () => {
+    test('test run', async ({ page }) => {
+        // create a new todo locator
+        const newTodo = page.getByPlaceholder('What needs to be done?');
+ await newTodo.fill(TODO_ITEMS[0])
+ await newTodo.press('Enter')
+
+    });
+  });
