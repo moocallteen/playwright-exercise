f1a901da3bb75e11746f59b32188371272006e7b
Dmytro Kukarin <Dmitry@attaro.co.uk>
2024-09-03 17:44:44 +0200

    guide 6 writing test suites

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // reporter: 'html',
+  reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -42,10 +43,10 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
     // {
     //   name: 'webkit',
---
+++ tests/guide-6/guide-6-writing_test_suits.spec.ts
@@ -0,0 +1,62 @@
+import { test, expect } from "@playwright/test";
+
+let baseUrl, firstNameText, lastNameText, addressText, numberValue, errorMessage, userDataSection;
+
+baseUrl =
+  "file:///Users/dmitrykukarin/Documents/AQA-files/play-playwright/tests/guide-6/index.html";
+
+const testData = {
+  firstName: "Test",
+  lastName: "Tester",
+  address: "Daisy Street",
+  number: "1413121110",
+};
+
+test.beforeEach(async ({ page }) => {
+  await page.setViewportSize({
+    width: 1920,
+    height: 1080,
+  });
+
+  await page.goto(baseUrl);
+});
+
+test.describe("User registration tests", () => {
+  test("Register with valid data", async ({ page }) => {
+    await page.fill(`input[id="firstName"]`, testData.firstName);
+    await page.fill(`input[id="lastName"]`, testData.lastName);
+    await page.fill(`input[id="address"]`, testData.address);
+    await page.fill(`input[id="number"]`, testData.number);
+    await page.click(`button[id="register"]`);
+
+    firstNameText = await page
+      .locator(`span[id="displayFirstName"]`)
+      .textContent();
+    lastNameText = await page
+      .locator(`span[id="displayLastName"]`)
+      .textContent();
+    addressText = await page.locator(`span[id="displayAddress"]`).textContent();
+    numberValue = await page.locator(`span[id="displayNumber"]`).textContent();
+
+    await expect(firstNameText).toEqual(testData.firstName);
+    await expect(lastNameText).toEqual(testData.lastName);
+    await expect(addressText).toEqual(testData.address);
+    await expect(numberValue).toEqual(testData.number);
+  });
+
+  test("Register some empty fields", async ({ page }) => {
+    await page.fill(`input[id="firstName"]`, testData.firstName);
+    await page.fill(`input[id="lastName"]`, testData.lastName);
+    await page.click(`button[id="register"]`);
+    errorMessage = await page.locator(`div[id="error"] p`).textContent();
+    expect(errorMessage).toBe("Please fill in all fields.");
+  });
+
+  test("Register all empty fields", async ({ page }) => {
+    await page.click(`button[id="register"]`);
+    errorMessage = await page.locator(`div[id="error"] p`).textContent();
+    expect(errorMessage).toBe("Please fill in all fields.");
+    userDataSection = await page.locator(`div[id="userData"]`);
+    expect(userDataSection).not.toContain("Registered Data");
+  });
+});
